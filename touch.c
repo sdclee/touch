495cba1508b72b97cdd6968f7f8a34c3b58eef91
Lee Crampton <sdclee@gmail.com>
2011-12-16 09:32:59 +0000

    Timestamp almost done

--- touch.c
+++ touch.c
@@ -74,34 +74,94 @@ int main (int argc, char **argv)
 		//tStamp = checkArgs (argv, '-t');
 		if (strcmp (argv [i], "-t") == 0)
 		{
+			// no safety check here yet !
 			char *tStamp = argv [i + 1];	
 			
 			printf ("timsestamp: %s\n", tStamp);	
+			//  [[CC]YY]MMDDhhmm[.ss] 
+			
+
+			struct tm
+			{
+				int tm_sec;      // 0 to 59 (or 60 for occasional rare leap-seconds)
+				int tm_min;      // 0 to 59
+				int tm_hour;     // 0 to 23
+				int tm_mday;     // 1 to 31
+				int tm_mon;      // 0 to 11, stupidly 0=January, 11=December
+				int tm_year;     // year-1900, so 79 means 1979, 103 means 2003
+				int tm_wday;     // 0 to 6, 0=Sunday, 1=Monday, ..., 6=Saturday
+				int tm_yday;     // 0 to 365, 0=1st January
+				int tm_isdst;    // 0 to 1, 1=DST is in effect, 0=it isn't
+				char *tm_zone;   // time zone, e.g. "PDT", "EST".
+				int tm_gmtoff;   // time zone in seconds from GMT; EST=-18000, WET=3600
+			} tm;
+
+			tm.tm_sec = 0;				
+			tm.tm_min = 0;				
+			
+			char szSec [3];
+			strcpy (szSec, "00");
+			
+			int tStampLen = strlen (tStamp);
+			printf ("tStampLen = %d\n", tStampLen);
+			
+			if (tStamp [tStampLen - 3] == '.')
+			{
+				printf ("Seconds specified\n");
+				strncpy (szSec, &tStamp [tStampLen - 2], 2);
+				
+				// then just lop off the end with a judiciously placed null to make the right hand end of the string
+				// work for MMDDhhmm
+				tStamp [tStampLen - 3] = '\0';
+				tStampLen = strlen (tStamp);
+			}
+			
+			char szMin [3];
+			strncpy (szMin, &tStamp [tStampLen - 2], 2);
+			szMin [2] = '\0';
+
+			char szHour [3];
+			strncpy (szHour, &tStamp [tStampLen - 4], 2);
+			szHour [2] = '\0';
+
+			char szDay [3];
+			strncpy (szDay, &tStamp [tStampLen - 6], 2);
+			szDay [2] = '\0';
+
+			char szMon [3];
+			strncpy (szMon, &tStamp [tStampLen - 8], 2);
+			szMon [2] = '\0';
+
+			tm.tm_sec   = atoi (szSec); 
+			tm.tm_min   = atoi (szMin); 
+			tm.tm_hour  = atoi (szHour); 
+			tm.tm_mday  = atoi (szDay); 
+			tm.tm_mon   = atoi (szMon) - 1;	// 0 = January 
+
+			char szYear [5];
+			memset (szYear, '\0', sizeof (szYear));
+			
+			// We could have 8, 10 or 12 chars left in the string from which to determine the year (and century)
+			if (strlen (tStamp) == 8)
+			{
+				// no CCYY specified		
+			}
+			else if (strlen (tStamp) == 10)
+			{
+				// just YY specified		
+				strncpy (szYear, tStamp, 2);
+				tm.tm_year = atoi (szYear);	
+			}
+			else if (strlen (tStamp) == 12)
+			{
+				// CCYY specified
+				strncpy (szYear, tStamp, 4);
+				tm.tm_year = atoi (szYear) - 1900;	
+			}			
+			printf ("year = %s month = %s day = %s hours = %s minutes = %s seconds = %s\n", szYear, szMon, szDay, szHour , szMin, szSec);		
 		}
 	}
-	
-/*
-    for (i = 1; i < argc; i++)  // Skip argv[0] (program name). 
-    {
-        //
-        // Use the 'strcmp' function to compare the argv values
-        // to a string of your choice (here, it's the optional
-        // argument "-q").  When strcmp returns 0, it means that the
-        // two strings are identical.
-        //
-
-        if (strcmp(argv[i], "-q") == 0)  // Process optional arguments. 
-        {
-            quiet = 1;  // This is used as a boolean value. 
-        }
-        else
-        {
-            // Process non-optional arguments here. 
-        }
-    }
-	
- */	
-	
+		
 	exit (0);
 }
 
